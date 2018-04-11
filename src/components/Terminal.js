ff2b296f61cfb1517f07546d43d23e1dae8bcaf9
Ben Christel <bchristel@pivotal.io>
2018-04-11 08:13:42 -0700

    Scroll to the bottom of the logs whenever the app view updates

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -3,24 +3,44 @@ import connectProps from './connectProps'
 import './Terminal.css'
 import Pane from './Pane'
 
-export default connectProps(props => {
-  let classNames = "Terminal"
-  if (props.appUi.screenLines.length) classNames += ' showScreen'
-  return (
-    <Pane style={{height: '600px', width: '100%', zIndex: 10}}>
-      <div className={classNames}>
-        <div className="logs">
-          {
-            props.appUi.logs.map((message, i) =>
-              <div className="log" key={i}>{message}</div>)
-          }
+export default connectProps(class extends React.Component {
+  bottomOfLogs = null
+
+  componentDidMount() {
+    this.scrollLogsToBottom()
+  }
+
+  componentDidUpdate() {
+    this.scrollLogsToBottom()
+  }
+
+  scrollLogsToBottom() {
+    if (this.bottomOfLogs) {
+      this.bottomOfLogs.scrollIntoView({behavior: 'smooth'})
+    }
+  }
+
+  render() {
+    let classNames = "Terminal"
+    if (this.props.appUi.screenLines.length) classNames += ' showScreen'
+    return (
+      <Pane style={{height: '600px', width: '100%', zIndex: 10}}>
+        <div className={classNames}>
+          <div className="logs">
+            {
+              this.props.appUi.logs.map((message, i) =>
+                <div className="log" key={i}>{message}</div>)
+            }
+            <div ref={e => this.bottomOfLogs = e}/>
+          </div>
+          <div className="screen">
+            {
+              this.props.appUi.screenLines.map((line, i) =>
+                <div className="screenLine" key={i}>{line}</div>)
+            }
         </div>
-        <div className="screen">
-          {
-            props.appUi.screenLines.map((line, i) =>
-              <div className="screenLine" key={i}>{line}</div>)
-          }
       </div>
-    </div>
-  </Pane>)
+    </Pane>)
+  }
+
 })
