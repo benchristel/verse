3a05d7db4d3e4594f5e196ddcbe8515dc2f90325
Ben Christel <bchristel@pivotal.io>
2018-04-13 08:26:40 -0700

    Tune linter options

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -14,6 +14,7 @@ export default connectProps(props => {
       theme="xcode"
       value={editorText(props)}
       onChange={props.changeEditorText}
+      onLoad={setLinterOptions}
       onBlur={save}
       name="AceEditor"
       editorProps={{$blockScrolling: true}}
@@ -27,3 +28,27 @@ export default connectProps(props => {
       editorText(props))
   }
 })
+
+function setLinterOptions(editor) {
+  let session = editor.getSession()
+  console.log(session.getMode())
+  if (session.$worker) {
+    session.$worker.send("setOptions", [{
+      curly: true,
+      eqeqeq: true,
+      esversion: 6,
+      funcscope: false,
+      futurehostile: true,
+      latedef: 'nofunc',
+      nocomma: true,
+      notypeof: true,
+      varstmt: true,
+      // warning codes from https://github.com/jshint/jshint/blob/2.1.4/src/shared/messages.js
+      '-W014': true, // allow operator-first style
+      '-W033': true, // suppress 'Missing semicolon'
+
+      // relaxing options
+      noyield: true,
+    }])
+  }
+}
