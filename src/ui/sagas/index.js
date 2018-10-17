9997024536d6bfba3b11843f713db0db66f3585e
Ben Christel <bchristel@pivotal.io>
2018-10-17 13:14:38 -0700

    Process file changes in a single pipeline saga
    - Files with syntax errors should not be saved

--- src/ui/sagas/index.js
+++ src/ui/sagas/index.js
@@ -3,19 +3,25 @@ import { fork, put, take, takeLatest, select } from 'redux-saga/effects'
 import { AnimationFrameTicker } from './AnimationFrameTicker'
 import { KeyEventStream } from './KeyEventStream'
 import { display, markSyntaxErrors, loadFiles } from '../actions'
-import { anySyntaxErrors, editorText } from '../selectors'
+import { editorText } from '../selectors'
 import { findSyntaxErrorLocations } from '../findSyntaxErrorLocations'
 import { Core } from '../../core'
 import storage from '../storage'
 
 const core = Core()
 
+// - save every file that has run and has no load errors
+// - files should have UUIDs. Names should be user-facing
+//   labels only. Names need not be unique.
+// - each file should have a state machine indicating whether it is loaded, saved.
+// saved (initial state) -[edit]-> dirty -> loaded -> saved
+// - if a file is dirty Verse should attempt to load it on any change to any file.
+// - likewise, if a file is loaded, Verse should attempt to save it whenever any files are saved.
+
 export function* main() {
   yield takeLatest('allowJsToRun', allowJsToRun)
   yield takeLatest('runApp', runApp)
-  yield takeLatest('changeEditorText', checkSyntax)
-  yield takeLatest('changeEditorText', deployFile)
-  yield takeLatest('changeEditorText', save)
+  yield takeLatest('changeEditorText', processFileChange)
   yield takeLatest('loadFiles', deployAllFiles)
   yield fork(animationFrameThread)
   yield fork(keyEventThread)
@@ -45,24 +51,38 @@ function *runApp() {
   yield put(display(view))
 }
 
-function *deployFile({text, file}) {
-  yield delay(15)
-  let view = core.deploy(file, text)
-  yield put(display(view))
-}
+function *processFileChange({text, file}) {
+  // TODO: yield put(markFile('dirty', file))
 
-function *save({text, file}) {
-  yield delay(1000)
-  if (yield select(anySyntaxErrors)) {
-    // don't save
+  /* Delay to allow the editor UI to update synchronously,
+   * without being blocked by code eval'ing. */
+  yield delay(1)
+
+  let view = core.deploy(file, text)
+  /* Deploying code may output errors or changes to the
+   * program's display, so send those to the UI. */
+  yield put(display(view))
+
+  if (view.syntaxErrors[file]) {
+    /* Give the user time to correct the error before
+     * distracting them with highlighting. */
+    yield delay(300)
+    let errors = findSyntaxErrorLocations(text)
+    yield put(markSyntaxErrors(errors))
+    /* Note that we don't save files with syntax errors.
+     * This means if you really screw something up you can
+     * refresh the page to get back to a state where the
+     * code compiles. */
   } else {
-    // TODO: what about other files that may be unsaved?
+    /* The code loaded successfully, so save it.
+     * Delay so we don't save too often. Browser storage
+     * can be pretty slow... */
+    yield delay(1000)
     storage.storeFile(file, text)
   }
 }
 
 function *deployAllFiles({files}) {
-  yield delay(0) // make the compiler happy
   if (files['main.js'] !== undefined) {
     let view = core.deploy('main.js', files['main.js'])
     yield put(display(view))
