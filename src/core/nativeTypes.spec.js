e6ea6ae6e13a184e09179d0d5749f84d634e89f2
Ben Christel <bchristel@pivotal.io>
2018-12-06 19:56:51 -0800

    Move tests for isIterator to the proper file

--- src/core/nativeTypes.spec.js
+++ src/core/nativeTypes.spec.js
@@ -1,4 +1,4 @@
-import { isObject, isArray } from './nativeTypes'
+import { isObject, isArray, isIterator } from './nativeTypes'
 
 describe('isObject', () => {
   it('returns true for an [object Object]', () => {
@@ -25,5 +25,21 @@ describe('isArray', () => {
 
   it('returns false for an object', () => {
     expect(isArray({})).toBe(false)
+  })
+})
+
+describe('isIterator', () => {
+  it('is false for a function', () => {
+    expect(isIterator(() => {})).toBe(false)
+  })
+
+  it('is true for a generator iterator', () => {
+    function *gen() {}
+    expect(isIterator(gen())).toBe(true)
+  })
+
+  it('is false for a generator function', () => {
+    function *gen() {}
+    expect(isIterator(gen)).toBe(false)
   })
 })
