61cf5b78a04f41794841dba3e5a5b769236d9a87
Ben Christel <bchristel@pivotal.io>
2018-12-22 09:53:11 -0800

    Remove unnecessary connectProps

--- src/ui/components/Backdrop.js
+++ src/ui/components/Backdrop.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import connectProps from './connectProps'
 import './Backdrop.css'
 
-export default connectProps(props => (
+export default props => (
   <div className="Backdrop">
     {props.children}
   </div>
-))
+)
